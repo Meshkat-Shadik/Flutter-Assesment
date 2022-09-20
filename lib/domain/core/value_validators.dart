49d0e194aa41735ab76eb5aa1db0f4e841c608e5
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2022-09-20 13:45:53 +0600

    typo fixed 😅

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -4,8 +4,8 @@ import 'package:ecommerce_demo/domain/failures/value_failures.dart';
 Either<ValueFailure<String>, String> validateSearchTerms({
   required String? input,
 }) {
-  const emailRegex = r'''^[a-zA-Z]*$''';
-  if (RegExp(emailRegex).hasMatch(input!) && input.isNotEmpty) {
+  const searchTermRegex = r'''^[a-zA-Z]*$''';
+  if (RegExp(searchTermRegex).hasMatch(input!) && input.isNotEmpty) {
     return right(input);
   } else {
     return left(
